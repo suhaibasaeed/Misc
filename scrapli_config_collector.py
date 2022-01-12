99e84d79cef20a606c61616452858fd521682647
Suhaib Saeed <63662275+suhaibasaeed@users.noreply.github.com>
2022-01-12 17:21:42 +0000

    Working version for NXOS on Linux

--- scrapli_config_collector.py
+++ scrapli_config_collector.py
@@ -1,10 +1,13 @@
-# Library used for async connections
 import asyncio
 import datetime
 import os
+from getpass import getpass
 # Import relevant driver
-from scrapli.driver.core import AsyncIOSXEDriver
+#from scrapli.driver.core import AsyncIOSXEDriver
 from scrapli.driver.core import AsyncNXOSDriver
+
+username = input("Please enter your username: ")
+password = getpass()
 
 def get_time():
     # Get the current time - This will be used in the file name
@@ -16,23 +19,30 @@ def get_time():
 
 def get_devices_file(device_file):
     with open(device_file) as f:
+        
         devices = f.read().splitlines()
+        # Remove whitespaces from lines and blank lines
+        devices = [i.strip() for i in devices]
+        devices = [x for x in devices if x]
 
         return devices
 
 def get_commands(commands_file):
     with open(commands_file) as f:
         commands = f.read().splitlines()
+        # Remove whitespaces from lines and blank lines
+        commands = [i.strip() for i in commands]
+        commands = [x for x in commands if x]
 
         return commands
 
 # Create co-routine which can be suspended and resumed
-async def gather_version(device, commands, time_now):
+async def gather_commands(device, commands, time_now):
     
     net_device = {
      "host": device,
-     "auth_username":"pyclass",
-     "auth_password": "88newclass",
+     "auth_username": username,
+     "auth_password": password,
      "auth_strict_key": False,
      "transport": "asyncssh",
     }
@@ -45,7 +55,7 @@ async def gather_version(device, commands, time_now):
     await connection.open()
     # Store result of prompt
     hostname = await connection.get_prompt()
-
+    # output/hostname
     complete_name = os.path.join('output', hostname)
 
     new_file = open(f"{complete_name}-{time_now}.txt", "a")
@@ -55,7 +65,11 @@ async def gather_version(device, commands, time_now):
     # Close connection
     await connection.close()
 
-    new_file.write(sh_result.result)
+    for i in range(len(sh_result)):
+        # Write command and output to file
+        new_file.write(f"{commands[i]}\n")
+        new_file.write(f"{sh_result[i].result}\n")
+        new_file.write(f"{'=' * 80}\n")
     new_file.close()
 
     return complete_name
@@ -71,17 +85,16 @@ async def main():
     time_nw = get_time()
 
     # List comprehension calling function/coroutine above
-    coroutines = [gather_version(device, commands, time_nw) for device in devices]
+    coroutines = [gather_commands(device, commands, time_nw) for device in devices]
     # Allows us to group together tasks
     results = await asyncio.gather(*coroutines)
 
     # Loop through tasks and print
     for result in results:
-        print(result)
+        result = result.split('/')[1].strip('#')
+        print(f"Commands from {result} were successfully collected")
 
 
 # Execute all of above - Pass in main coroutine
 # Create event loop and run
 asyncio.run(main())
-
-
